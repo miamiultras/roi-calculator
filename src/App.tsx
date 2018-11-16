a54cdac07eafba50e6e5d358ea348a7e46ab30d3
Maciej Szewczyk <maciej.szewczyk92@gmail.com>
2018-11-16 19:45:04 +0100

    setup typography and prototype landing page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,20 @@ class App extends Component {
       <div className="banner">
         <div className="gradient">
           <div className="title-container">
-            title
+            <div className="title">
+              <h1>Live PPV Event ROI Calculator</h1>
+            </div>
+            <div className="description">
+              Ever wondered <strong>how much money you can generate </strong>
+              with the Live Pay-Per-View model? Based on our experience in
+              <strong> managing over 10,000 PPV</strong> events per year,
+               we created this unique ROI calculator for you.
+            </div>
+            <a href="">
+              <button>
+                Give it a try!
+              </button>
+            </a>
           </div>
         </div>
       </div>
